2d97cd7c5018c0d85e2e2cd09855c1f9e6bd491c
sh5452 <105583484+sh5452@users.noreply.github.com>
2026-05-15 00:15:19 +0300

    a

--- server/index.js
+++ server/index.js
@@ -46,6 +46,23 @@ app.get('/orders', async (req,res)=>{
     }
 })
 
+app.post('/order-items', async(req,res)=>{
+    try{
+        const{order_id, product_name, sku, quantity, unit_type, price}=req.body
+        const result=await pool.query(
+            `INSERT INTO order_items(order_id, product_name, sku, quantity, unit_type, price)
+            VALUES ($1,$2,$3,$4,$5,$6)
+            RETURNING *`
+            [order_id,product_name,sku, quantity, unit_type, price]
+            
+        )
+        res.json(result.rows[0])
+    }catch(err){
+        console.error(err)
+        res.status(500).send('ERROR addind item')
+    }
+})
+
 app.listen(5000,()=>{
     console.log("Srever is running in port 5000")
 })
