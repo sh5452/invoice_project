1074e654a9fe319ba3a6363aa14c9fe5a088c9c4
sh5452 <105583484+sh5452@users.noreply.github.com>
2026-07-16 15:54:59 +0300

    a

--- server/db/return_items.sql
+++ server/db/return_items.sql
@@ -1,7 +1,7 @@
 CREATE TABLE return_items(
     id SREIAL PRIMARY KEY,
     return_id INT NOT NULL
-    REFERENCES return_items(id),
+    REFERENCES returns(id),
     order_items_id INT NOT NULL
     REFERENCES order_items(id),
     quantity_returned INT NOT NULL
