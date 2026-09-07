860fa768841a61622bb35a7735ff57528faba5dc
sh5452 <105583484+sh5452@users.noreply.github.com>
2026-09-07 13:42:06 +0300

    a

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('Invoice server is running!');
+});
 
 // =========================
 // Routes
