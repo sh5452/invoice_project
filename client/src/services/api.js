e7fa1b1b4b5f715f31bb6603676be7c5e2cca403
sh5452 <105583484+sh5452@users.noreply.github.com>
2026-09-07 13:45:41 +0300

    a

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL:"https://invoice-project-3.onrender.com"
 });
 api.interceptors.request.use((config) => {
 
